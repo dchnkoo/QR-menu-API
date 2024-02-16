8c386f5bfba66baee7df0a367c14c6f1d6d3c48c
Valerii Diachenko <valera.dchn@gmail.com>
2024-02-16 20:50:49 +0200

    recovery codes saved in redis

--- qr-sys/app/framework/redis/__init__.py
+++ qr-sys/app/framework/redis/__init__.py
@@ -4,7 +4,7 @@ import redis
 import os
 
 
-def get_redis_connection(db: int):
+def get_redis_connection(db: int) -> redis.Redis:
     return redis.from_url(
         f"redis://{REDIS_HOST if DEBUG else os.environ.get('REDIS_HOST')}:{REDIS_PORT if DEBUG else os.environ.get('REDIS_PORT')}/{db}"
     )
